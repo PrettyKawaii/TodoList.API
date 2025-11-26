996f48c721c0b81f654c7ceb13b81840c8a2113d
PrettyKawaii <litvinocih@gmail.com>
2025-11-26 19:58:19 +0300

    Playing with MapGet, beginning to grasp something!

--- TodoList.API/Program.cs
+++ TodoList.API/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Greetings! Soon enought I'll rule the world!");
+app.MapGet("/", () => "Greetings! Soon enough I'll rule the world!");
+app.MapGet("/map_get", () => "A bit of testing how .MapGet() works.");
 
 app.Run();
